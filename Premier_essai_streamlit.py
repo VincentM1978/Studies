21a106851823a7295c6a538d589205661300ddb8
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 16:09:31 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -24,3 +24,14 @@ hobby = st.selectbox("Hobbies: ",
  
 # print the selected hobby
 st.write("Your hobby is: ", hobby)
+
+
+# multi select box
+ 
+# first argument takes the box title
+# second argument takes the options to show
+hobbies = st.multiselect("Hobbies: ",
+                         ['Dancing', 'Reading', 'Sports'])
+ 
+# write the selected options
+st.write("You selected", len(hobbies), 'hobbies')
