ca04607b18094b1677b6b672d7e983b76130f2a4
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 12:13:25 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -1,8 +1,14 @@
-import streamlit as st
-
-st.title('RIMOUVEUR DE LA MORT')
+st.title("Détourage d'image")
 
 image_upload = st.file_uploader("Choisissez une image", type = ['png', 'jpg', 'jpeg'])
 
+def convert_image(image):
+  buf = BytesIO()
+  image.save(buf, format='PNG')
+  byte_im = buf.getvalue()
+  return byte_im
+
 if image_upload:
-  st.download_button("Télécharger l'image", image_upload, "imagedl.jpg", mime="image")
+  image = Image.open(image_upload)
+  fixed = remove(image)
+  downloadable_image = convert_image(fixed)
