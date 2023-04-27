26559ccae762b388714626d65c984625f9f6e390
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 16:12:46 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -1,40 +1,17 @@
 import streamlit as st
 
 
-st.title('toto')
-
-# radio button
-# first argument is the title of the radio button
-# second argument is the options for the ratio button
-status = st.radio("Select Gender: ", ('Male', 'Female'))
- 
-# conditional statement to print
-# Male if male is selected else print female
-# show the result using the success function
-if (status == 'Male'):
-    st.success("Male")
-else:
-    st.success("Female")
+st.title('Outil de recommandation de films')
 
   
   
-  # Selection box
- 
-# first argument takes the titleof the selectionbox
-# second argument takes options
-hobby = st.selectbox("Hobbies: ",
-                     ['Dancing', 'Reading', 'Sports'])
- 
-# print the selected hobby
-st.write("Your hobby is: ", hobby)
-
 
 # multi select box
  
 # first argument takes the box title
 # second argument takes the options to show
-hobbies = st.multiselect("Hobbies: ",
-                         ['Dancing', 'Reading', 'Sports'])
+genres = st.multiselect("Genres: ",
+                         ['Drama', 'Action', 'Fantasy'])
  
 # write the selected options
-st.write("You selected", len(hobbies), 'hobbies')
+st.write("You selected", len(genres), 'genres')
