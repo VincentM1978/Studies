abc451c59f52a4ba2dfe5af33f4635122535945e
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 16:15:21 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -15,3 +15,16 @@ genres = st.multiselect("Genres: ",
  
 # write the selected options
 st.write("You selected", len(genres), 'genres')
+
+
+# slider
+ 
+# first argument takes the title of the slider
+# second argument takes the starting of the slider
+# last argument takes the end number
+level = st.slider("Sélectionnez une note minimale de film", 1, 10)
+ 
+# print the level
+# format() is used to print value
+# of a variable at a specific position
+st.text('Selected: {}'.format(level))
