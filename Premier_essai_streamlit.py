18dec6c295bce25ec08c3c2c677b81526e8bbcaa
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 16:21:52 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -4,7 +4,18 @@ import streamlit as st
 st.title('Outil de recommandation de films')
 
   
-  
+ # Text Input
+ 
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+name = st.text_input("Enter a name", "Type Here ...")
+ 
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+    result = name.title()
+    st.success(result) 
 
 # multi select box
  
