02cff032b67336cf1b7fa116fd4a9ef63cbee2d5
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 16:08:21 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -12,3 +12,15 @@ if (status == 'Male'):
     st.success("Male")
 else:
     st.success("Female")
+
+  
+  
+  # Selection box
+ 
+# first argument takes the titleof the selectionbox
+# second argument takes options
+hobby = st.selectbox("Hobbies: ",
+                     ['Dancing', 'Reading', 'Sports'])
+ 
+# print the selected hobby
+st.write("Your hobby is: ", hobby)
