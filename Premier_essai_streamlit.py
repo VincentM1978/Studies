9903fa9fa586793decbf4aa6a98f6d0f56eb959a
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 11:47:02 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -1,1 +1,8 @@
-print('Hello World')
+import streamlit as st
+
+st.title('RIMOUVEUR DE LA MORT')
+
+image_upload = st.file_uploader("Choisissez une image", type = ['png', 'jpg', 'jpeg'])
+
+if image_upload:
+  st.download_button("Télécharger l'image", image_upload, "imagedl.jpg", mime="image")
