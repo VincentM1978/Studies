e023dd5408445be369158dc473a147dd03a91b72
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-04-27 16:07:17 +0200

    Update Premier_essai_streamlit.py

--- Premier_essai_streamlit.py
+++ Premier_essai_streamlit.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from PIL import Image
 
-from io import BytesIO
-
-st.title("Détourage d'image")
-
-image_upload = st.file_uploader("Choisissez une image", type = ['png', 'jpg', 'jpeg'])
-
-def convert_image(image):
-  buf = BytesIO()
-  image.save(buf, format='PNG')
-  byte_im = buf.getvalue()
-  return byte_im
+# radio button
+# first argument is the title of the radio button
+# second argument is the options for the ratio button
+status = st.radio("Select Gender: ", ('Male', 'Female'))
+ 
+# conditional statement to print
+# Male if male is selected else print female
+# show the result using the success function
+if (status == 'Male'):
+    st.success("Male")
+else:
+    st.success("Female")
